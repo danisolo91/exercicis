f3e063fd11ca13ceb1e5c1e2f1b278d1cddb727a
Daniel <danisolo91@gmail.com>
2021-07-20 22:39:37 +0200

    Delete fases 1 and 2

--- modul7/Nivell2Fase1/src/com/vehicles/project/Truck.java
+++
@@ -1,9 +0,0 @@
-package com.vehicles.project;
-
-public class Truck extends Vehicle {
-
-	public Truck(String plate, String brand, String color) {
-		super(plate, brand, color);
-	}
-
-}
--- modul7/Nivell2Fase2/src/com/vehicles/project/Car.java
+++
@@ -1,9 +0,0 @@
-package com.vehicles.project;
-
-public class Car extends Vehicle {
-
-	public Car(String plate, String brand, String color) {
-		super(plate, brand, color);
-	}
-
-}
